ba34efaca1c097b66f8fe5d1c188dcf7c41061a4
mervis mascarenhas <mervismascarenhas@gmail.com>
2024-07-13 02:02:43 +0530

    Aroma Food Ordering App

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,20 @@
 import RestaurantCard from "./RestaurantCard";
 import Reslist from "../utils/mockdata";
+import { useState } from "react";
+
+
 const Body = () => {
+    const [listRes, setRes] = useState(Reslist);
     return (
         <div className="body">
-            <div className="search">search</div>
+            <button className="filter-btn" onClick={() => {
+                const filterList = listRes.filter(
+                    (res) => res.info.avgRating >= 4.6
+                )
+                setRes(filterList);
+            }}>Top Rated Restaurants</button>
             <div className="res-container">
-                {Reslist.map((item) => (
+                {listRes.map((item) => (
                     <RestaurantCard key={item.info.id} Resdata = {item}/>
                 ))}
             
